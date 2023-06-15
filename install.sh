be0c7616d5dcf08b666cd1202563fbab381b8096
Gethe <gethe88@gmail.com>
2023-06-15 18:53:21 -0400

    Make custom.css editable

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 
 mkdir /home/pi/printer_data/config/.theme
 ln -sf /home/pi/mainsail_theme/.theme/* /home/pi/printer_data/config/.theme/
+
+echo "@import \"custom_base.css\"" >/home/pi/printer_data/config/.theme/custom.css
